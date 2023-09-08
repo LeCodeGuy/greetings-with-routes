278331541eb813d5336fe909095f821a953196e4
LeCodeGuy <devanj89@gmail.com>
2023-09-08 15:08:24 +0200

    made changes to connection settings

--- routes/database.js
+++ routes/database.js
@@ -30,8 +30,8 @@ const connection = {
 const connectionString= process.env.CONNECTION_STRING;
 
 // Instaniate Database
-//const database = pgp(connection);
-const database = pgp(connectionString);
+const database = pgp(connection);
+//const database = pgp(connectionString);
 
 // Open the connection to the database
 database.connect();
