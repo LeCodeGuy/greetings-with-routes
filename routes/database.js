c3c89a7062bd591279cdcd64f10f8dcc9cb1cbb1
LeCodeGuy <devanj89@gmail.com>
2023-09-08 10:46:58 +0200

    reverted back to connection string

--- routes/database.js
+++ routes/database.js
@@ -9,26 +9,30 @@ let pgp = pgPromise();
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
+let myPort = 3000;
 // if we have a DB URL and the env is not local use SSL
 if (process.env.DATABASE_URL && !local) {
     useSSL = true;
+    myPort = process.env.DATABASE_PORT;
 }
+
 // Connection object for pg-promise
 const connection = {
     host:process.env.DATABASE_HOST,
-    port:process.env.DATABASE_PORT,
+    port:myPort,
     database:process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    ssl: true
+    ssl: useSSL,
 
 }
 // which db connection to use
 const connectionString= process.env.CONNECTION_STRING;
-// console.log(connection);
+//console.log(connection);
+//console.log(connectionString);
 // Instaniate Database
-const database = pgp(connection);
-//const database = pgp(connectionString);
+//const database = pgp(connection);
+const database = pgp(connectionString);
 
 // Open the connection to the database
 database.connect();
